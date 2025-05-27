0844fa3893e6250579d1854fab77d73b4d1da902
Anandhu-ms7 <anandhusunilktyr@gmail.com>
2025-05-27 21:18:32 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,10 @@
+console.log("");
+
+var contact_btn = document.getElementById("contact_btn");
+contact_btn.addEventListener("click", function() {
+    window.open("https://wa.link/t2bz93");
+
+
+
+})
+
